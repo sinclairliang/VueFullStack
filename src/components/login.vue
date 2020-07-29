76049d099fbfceba03144a31715541d678532bfa
Sinclair Liang <sinclairliang@gmail.com>
2020-07-29 04:03:04 -0500

    added vue component

---
+++ src/components/login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    USERNAME:
+    <input type="text" v-model="loginForm.username" placeholder="Enter your username" />
+    <br />
+    <br />Password:
+    <input type="password" v-model="loginForm.password" placeholder="Password" />
+    <br />
+    <br />
+    <button v-on:click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      responseResult: []
+    };
+  },
+  methods: {
+    login() {
+      this.$axios
+        .post("/login", {
+          username: this.loginForm.username,
+          password: this.loginForm.password
+        })
+        .then(successResponse => {
+          if (successResponse.data.code === 200) {
+            this.$router.replace({ path: "/index" });
+          }
+        })
+        .catch(failResponse => {});
+    }
+  }
+};
+</script>
