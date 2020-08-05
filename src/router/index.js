656ca0242ac2072a4903924d567d77a0003be41b
Sinclair Liang <sinclairliang@gmail.com>
2020-08-04 23:35:25 -0500

    fresh start

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import AppIndex from '@/components/home/AppIndex'
-import Login from '@/components/Login'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', redirect: '/login' },
-    { path: '/login', name: 'Login', component: Login },
-    { path: '/index', name: 'AppIndex', component: AppIndex }
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
   ]
 })
