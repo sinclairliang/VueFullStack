aaa4eb46ba37656ee857a2c4242d12bab7919459
Sinclair Liang <sinclairliang@gmail.com>
2020-08-13 23:30:43 -0500

    added new functions to side menu

--- src/components/library/SideMenu.vue
+++ src/components/library/SideMenu.vue
@@ -33,7 +33,18 @@
 
 <script>
 export default {
-  name: 'SideMenu'
+  name: 'SideMenu',
+  data () {
+    return {
+      cid: ''
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      this.cid = key
+      this.$emit('indexSelect')
+    }
+  }
 }
 </script>
 
