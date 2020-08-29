db53a336da036af94e2548554a6a311a8c8489fa
Sinclair Liang <sinclairliang@gmail.com>
2020-08-28 20:11:50 -0500

    writing on admins

---
+++ src/components/admin/AdminMenu.vue
@@ -0,0 +1,30 @@
+  <template>
+  <el-menu
+    :default-active="currentPath"
+    class="el-menu-admin"
+    router
+    mode="vertical"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    :collapse="isCollapse"
+  >
+    <div style="height: 80px;"></div>
+    <el-submenu
+      v-for="(item,i) in adminMenus"
+      :key="i"
+      :index="(i).toString()"
+      style="text-align: left"
+    >
+      <span slot="title" style="font-size: 17px;">
+        <i :class="item.iconCls"></i>
+        {{item.nameZh}}
+      </span>
+      <el-menu-item v-for="child in item.children" :key="child.path" :index="child.path">
+        <i :class="child.icon"></i>
+        {{ child.nameZh }}
+      </el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
