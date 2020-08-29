79187d6bbda889d12d31aa77a802205680e35075
Sinclair Liang <sinclairliang@gmail.com>
2020-08-28 20:01:12 -0500

    writing on admins

--- src/router/index.js
+++ src/router/index.js
@@ -55,3 +55,67 @@ export default new Router({
     }
   ]
 })
+
+export const createRouter = routes =>
+  new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: '/',
+        name: 'Default',
+        redirect: '/home',
+        component: Home
+      },
+      {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        redirect: '/index',
+        children: [
+          {
+            path: '/index',
+            name: 'AppIndex',
+            component: () => import('../components/home/AppIndex')
+          },
+          {
+            path: '/library',
+            name: 'Library',
+            component: () => import('../components/library/LibraryIndex')
+          }
+        ]
+      },
+      {
+        path: '/login',
+        name: 'Login',
+        component: () => import('../components/Login')
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: () => import('../components/Register')
+      },
+      {
+        path: '/admin',
+        name: 'Admin',
+        component: () => import('../components/admin/AdminIndex'),
+        meta: {
+          requireAuth: true
+        },
+        children: [
+          {
+            path: '/admin/dashboard',
+            name: 'Dashboard',
+            component: () =>
+              import('../components/admin/dashboard/admin/index'),
+            meta: {
+              requireAuth: true
+            }
+          }
+        ]
+      },
+      {
+        path: '*',
+        component: () => import('../components/pages/Error404')
+      }
+    ]
+  })
