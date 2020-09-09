6757b5a1b86f00f238ea2dcea0f31a4929337a33
Sinclair Liang <sinclairliang@gmail.com>
2020-09-09 01:35:02 -0500

    added Error 404 contents

---
+++ src/components/pages/Error404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="error-page">
+    <div style="padding:100px 500px 0px 0px">
+      <p>Error</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error404'
+}
+</script>
+
+<style scoped>
+.error-page {
+  background-position: center;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: fixed;
+}
+</style>
